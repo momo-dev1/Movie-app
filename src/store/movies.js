5cf10605729fffeb44c101ecbd737debc8614341
momo-dev1 <didy1234567@gmail.com>
2022-10-04 02:57:39 +0200

    getMovieByID

--- src/store/movies.js
+++ src/store/movies.js
@@ -30,5 +30,17 @@ export const useMoviesStore = defineStore("movies", {
                 [this.isLoading, this.loadingMessage] = [true, err.message];
             }
         },
+        async getMovieByID(id) {
+            this.isLoading = true;
+            try {
+                const { data, status } = await axios.get(`${ import.meta.env.VITE_API_URL }?apikey=${ import.meta.env.VITE_API_KEY }&i=${ id }`);
+                if (status != 200) {
+                    throw new Error(data.Error);
+                }
+                [this.movie, this.isLoading] = [data, false];
+            } catch (err) {
+                console.log(err.message);
+            }
+        },
     },
 });
