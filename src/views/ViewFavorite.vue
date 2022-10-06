0db7533c4b001029f2b1b3c2f659450c4f8d7753
momo-dev1 <didy1234567@gmail.com>
2022-10-06 03:41:18 +0200

    favourites component

---
+++ src/views/ViewFavorite.vue
@@ -0,0 +1,25 @@
+<script setup>
+import { onMounted } from "@vue/runtime-core";
+import MoviesList from "@/components/MoviesList.vue";
+import Loading from "@/components/Loading.vue";
+import { useFavoritStore } from "@/store/favourite";
+import { useMoviesStore } from "@/store/movies";
+const store = useFavoritStore();
+const moviesStore = useMoviesStore();
+
+onMounted(() => {
+  if (store.favMovies.length < 1) {
+    moviesStore.isLoading = true;
+    moviesStore.loadingMessage = "Empty";
+  }
+});
+</script>
+    
+<template>
+  <main>
+    <h3 class="text-gray-500 text-xl tracking-widest">Your Favorite Movies</h3>
+    <MoviesList :movies="store.favMovies" />
+    <Loading v-if="moviesStore.isLoading" />
+  </main>
+</template>
+    
